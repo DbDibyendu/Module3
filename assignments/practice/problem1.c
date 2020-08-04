9f30262468942dd48550b9a265b288ba11c4c49d
Dibyendu <dbdibyendu5@gmail.com>
2020-08-04 21:31:05 +0530

    updated

--- assignments/practice/problem1.c
+++ assignments/practice/problem1.c
@@ -1,10 +1,11 @@
-/** @file <file_name>.c
- *  @brief Give a description of the file 
+/** @file problem1.c
+ *  @brief HeartBeat
  *  
- *  Give full description of the file 
+ *  Getting Important Infomation from heartbeat device and uploading it to the MQQT Server 
+ *  in JSON format to the client
  *  
- *  @author Full name of the author 
- *  @bug List Any bugs found in the file  
+ *  @author Dibyendu Biswas
+ *  @bug I didn't find any from my side 
  */
 
 /*
@@ -19,7 +20,20 @@
 
 /* --- Standard Includes --- */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h> /* for strncpy */
+#include <errno.h>
+#include <time.h>
+#include "MQTTClient.h"
 
+/* includes for ipAddress */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
 /* --- Project Includes --- */
 
 
@@ -31,34 +45,214 @@
  *#####################################################################
  */
 
-/** 
- *  @brief Description on function_1
- *  
- *  Full description of the function
- *
- *  @return List all Function returns 
- */
+/* Declaring Global Variables */
 
-void function_1 (void)
+#define CLIENTID    "HEARTBEAT_Information"
+#define TOPIC       "MQTT"
+#define QOS         1
+#define TIMEOUT     10000L
+
+char DeviceId[10];                               // for storing the devicId address location
+
+
+/************************** 
+ * Error-handling functions
+ **************************/
+/* $begin errorfuns */
+/* $begin unixerror */
+void unix_error(char *msg) /* unix-style error */
 {
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+/* $end unixerror */
 
-        /*Make sure you comment every line */
-	
+void posix_error(int code, char *msg) /* posix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(code));
+    exit(0);
 }
 
+void app_error(char *msg) /* application error */
+{
+    fprintf(stderr, "%s\n", msg);
+    exit(0);
+}
+/* $end errorfuns */
+
+FILE *Fopen(const char *filename, const char *mode)
+{
+   FILE *fp;
+
+   if ((fp = fopen(filename, mode)) == NULL)
+   unix_error((char*)"Fopen error");
+
+   return fp;
+}
+
+void Fclose(FILE *fp) 
+{
+    if (fclose(fp) != 0)
+    unix_error((char*)"Fclose error");
+}
+
+
+
 /** 
- *  @brief Description on main
+ *  @brief IpAddress
  *  
- *  Full description of the function
+ *  finds out IpAddress of system
+ *
+ *  @return returns IpAddres
+ */
+
+const char *IP_ADDRESS()
+{
+ int fd;
+ struct ifreq ifr;
+
+ fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+ /* I want to get an IPv4 IP address */
+ ifr.ifr_addr.sa_family = AF_INET;
+
+ /* I want IP address attached to "eth0" */
+ strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
+
+ ioctl(fd, SIOCGIFADDR, &ifr);
+
+ close(fd);    
+
+// returning ipAddress
+ return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+
+}
+
+
+/** 
+ *  @brief DeviceId
+ *  
+ *  Gets the deviceId
+ *
+ *  @return returns deviceId
+ */
+
+const char *DEVICE_ID(){
+    FILE *fp=Fopen("/etc/shunya/deviceid","rb");
+    fscanf(fp, "%[^\n]", DeviceId);
+    return DeviceId;
+}
+
+/* Important Functions for asynchronous publication on MQTT Server  */
+
+volatile MQTTClient_deliveryToken deliveredtoken;
+void delivered(void *context, MQTTClient_deliveryToken dt)
+{
+    printf("Message with token value %d delivery confirmed\n", dt);
+    deliveredtoken = dt;
+}
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+    int i;
+    char* payloadptr;
+    printf("Message arrived\n");
+    printf("     topic: %s\n", topicName);
+    printf("   message: ");
+    payloadptr = message->payload;
+    for(i=0; i<message->payloadlen; i++)
+    {
+        putchar(*payloadptr++);
+    }
+    putchar('\n');
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+
+void connlost(void *context, char *cause)
+{
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
+}
+
+
+/** 
+ *  @brief Publishing MQTT in JSON format
+ *  
+ *  Getting all the details through different functions and connecting to the MQTT server to send
+ *  the message to the client in JSON format
  *
  *  @return List all Function returns 
  */
 
 int main (void)
 {
+    // Declaring all the variables
+        unsigned long timestamp=(unsigned long)time(NULL);                         //getting the value of timestamp
+        char eventType[10]="heartbeat";                                 // event type is fixed
+        char deviceId[20];                                                          // creating char for storing deviceId
+        char ipAddress[10];                                                     // creating char for storing ipAddress
+        char time[10];                                                      // creaating char for timestamp
+        strcpy(ipAddress,IP_ADDRESS());                                          // copying the address to the char variable
+        sprintf(time, "%ld", timestamp);                                        // converting long to string
+        int x;
+        char url[100];
 
-        /*Make sure you comment every line */
-        return 0;
-        
+    // Getting User input about the x interval and server address
+        printf("\nEnter value of x in seconds to send in interval:");
+        scanf("%d",&x);
+        printf("\nEnter URL of MQQT server:");
+        scanf("%s",url);
+
+    // Printing all the values
+        printf("Address: \"%s\"\n",ipAddress);                             
+        printf("device: \"%s\"\n",eventType);
+        printf("timestamp: %s\n",time);
+    //    strcpy(deviceId,DEVICE_ID());
+        printf("deviceId: \"%s\"\n",deviceId);
+    
+    // Creating message which will be sent to the MQTT Server
+
+        char msg[1000]="{\n    \"ipAddress\": \"";
+        strcat(msg,ipAddress);
+        strcat(msg,"\", \n    \"timestamp\": ");
+        strcat(msg,time);
+        strcat(msg,",\n    \"eventType\": \"");
+        strcat(msg,eventType);
+        strcat(msg,"\", \n    \"deviceId\": \"");
+        strcat(msg,deviceId);
+        strcat(msg,"\"\n}");
+
+    // uploading the data to MQTT server
+
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+    int rc;
+    MQTTClient_create(&client, url, CLIENTID,MQTTCLIENT_PERSISTENCE_NONE, NULL);                    // creating the Client with given url
+    conn_opts.keepAliveInterval = x;                                            // time in seconds to send the data in intervals
+    conn_opts.cleansession = 1;
+    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(EXIT_FAILURE);
+    }
+    pubmsg.payload = msg;
+    pubmsg.payloadlen = strlen(msg);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    deliveredtoken = 0;
+    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+    printf("\nWaiting for publication of\n %s\n"  "on topic %s for client with ClientID: %s\n", msg, TOPIC, CLIENTID);   // publishing the message if connected
+    while(deliveredtoken != token);
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
+    
+    // Compile and run using command gcc problem1.c -lpaho-mqtt3c
 }
+
 
--- assignments/practice/problem2.c
+++ assignments/practice/problem2.c
@@ -1,10 +1,10 @@
-/** @file <file_name>.c
- *  @brief Give a description of the file 
+/** @file problem2.c
+ *  @brief Barcode reader
  *  
- *  Give full description of the file 
+ *  Reading the barcode and printing it
  *  
- *  @author Full name of the author 
- *  @bug List Any bugs found in the file  
+ *  @author Dibyendu 
+ *  @bug I didn't find any  
  */
 
 /*
@@ -19,8 +19,100 @@
 
 /* --- Standard Includes --- */
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
 /* --- Project Includes --- */
+
+/* Global Variables */
+ char api_key[100]= "";                      // https://upcdatabase.org/
+ char ss[100]="";
+
+/************************** 
+ * Error-handling functions
+ **************************/
+/* $begin errorfuns */
+/* $begin unixerror */
+void unix_error(char *msg) /* unix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+/* $end unixerror */
+
+void posix_error(int code, char *msg) /* posix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(code));
+    exit(0);
+}
+
+void app_error(char *msg) /* application error */
+{
+    fprintf(stderr, "%s\n", msg);
+    exit(0);
+}
+/* $end errorfuns */
+
+/******************************************
+ * Wrappers for the Standard I/O functions.
+ ******************************************/
+void Fclose(FILE *fp) 
+{
+    if (fclose(fp) != 0)
+    unix_error((char*)"Fclose error");
+}
+
+FILE *Fdopen(int fd, const char *type) 
+{
+    FILE *fp;
+
+    if ((fp = fdopen(fd, type)) == NULL)
+    unix_error((char*)"Fdopen error");
+
+    return fp;
+}
+
+char *Fgets(char *ptr, int n, FILE *stream) 
+{
+    char *rptr;
+
+    if (((rptr = fgets(ptr, n, stream)) == NULL) && ferror(stream))
+    app_error((char*)"Fgets error");
+
+    return rptr;
+}
+
+FILE *Fopen(const char *filename, const char *mode) 
+{
+    FILE *fp;
+
+    if ((fp = fopen(filename, mode)) == NULL)
+    unix_error((char*)"Fopen error");
+
+    return fp;
+}
+
+void Fputs(const char *ptr, FILE *stream) 
+{
+    if (fputs(ptr, stream) == EOF)
+    unix_error((char*)"Fputs error");
+}
+
+size_t Fread(void *ptr, size_t size, size_t nmemb, FILE *stream) 
+{
+    size_t n;
+
+    if (((n = fread(ptr, size, nmemb, stream)) < nmemb) && ferror(stream)) 
+    unix_error((char*)"Fread error");
+    return n;
+}
+
+void Fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) 
+{
+    if (fwrite(ptr, size, nmemb, stream) < nmemb)
+    unix_error((char*)"Fwrite error");
+}
 
 
 /*
@@ -32,18 +124,85 @@
  */
 
 /** 
- *  @brief Description on function_1
+ *  @brief Barcode Reader
  *  
  *  Full description of the function
  *
  *  @return List all Function returns 
  */
 
-void function_1 (void)
-{
+  char *barcode_reader(){
+      char hid[52][2] = { 4,'a', 5, 'b', 6, 'c', 7, 'd', 8, 'e', 9, 'f', 10, 'g', 11, 'h', 12, 'i', 13, 'j', 14, 'k', 15, 'l', 16, 'm',
+           17, 'n', 18, 'o', 19, 'p', 20, 'q', 21, 'r', 22, 's', 23, 't', 24, 'u', 25, 'v', 26, 'w', 27, 'x', 28, 'y',
+           29, 'z', 30, '1', 31, '2', 32, '3', 33, '4', 34, '5', 35, '6', 36, '7', 37, '8', 38, '9', 39, '0', 44, ' ',
+           45, '-', 46, '=', 47, '[', 48, ']', 49, '\\', 51, ';', 52, '\'', 53, '~', 54, ',', 55, '.', 56, '/'};
 
-        /*Make sure you comment every line */
-	
+      char  hid2[52][2] = {4, 'A', 5,'B', 6,'C', 7 ,'D', 8, 'E', 9, 'F', 10, 'G', 11, 'H', 12, 'I', 13, 'J', 14, 'K', 15, 'L', 16, 'M',
+            17, 'N', 18, 'O', 19, 'P', 20, 'Q', 21, 'R', 22, 'S', 23, 'T', 24, 'U', 25, 'V', 26, 'W', 27, 'X', 28, 'Y',
+            29, 'Z', 30, '!', 31, '@', 32, '#', 33, '$', 34, '%', 35, '^', 36, '&', 37, '*', 38, '(', 39, ')', 44, ' ',
+            45, '_', 46, '+', 47, '{', 48, '}', 49, '|', 51, ',', 52, '"', 53, '~', 54, '<', 55, '>', 56, '?'};
+        
+        FILE *fp= Fopen("/dev/hidraw0","rb");
+        bool shift = false;
+        bool done = false;
+        while(!done){
+            // Get character from the HID
+            char buffer[100];
+            Fgets(buffer,8,fp);
+            int l=strlen(buffer);
+            for(int i=0;i<l;i++){
+                if(buffer[i]>0){
+                    // 40 is carriage return which signifies
+                    // we are done looking for characters
+                    if(buffer[i]==40){
+                        done =true;
+                        break;
+                    }
+                    // If we are shifted then we have to 
+                    // use the hid2 characters.
+                    if (shift){
+                        // If it is a '2' then it is the shift key
+                        if(buffer[i]==2){
+                            shift = true;
+                        }
+                        else
+                        {
+                            strcat(ss,hid2[buffer[i]][1]);
+                            shift = false;
+                        }
+                    }
+                    // if we are not shifted then use   
+                    // the hid characters
+
+                    else{
+
+                        // if it is a '2 then it is the shift key      
+                        if(buffer[i]==2){
+                            shift =true;
+                        }
+                        // if not 2 then lookup in the map
+                        else
+                        {
+                            strcat(ss,hid[buffer[i]][1]);
+                        }
+
+                        
+                    }      
+                }
+            }
+        }
+    return ss;
+}
+/** 
+ *  @brief Barcode Reader
+ *  
+ *  Full description of the function
+ *
+ *  @return List all Function returns 
+ */
+
+void UPC_Lookup(char *api_key,char *upc){
+
 }
 
 /** 
@@ -56,9 +215,22 @@ void function_1 (void)
 
 int main (void)
 {
+      char buffer[100]="dibyendu";
+      strcpy(buffer,barcode_reader());
+      int l=strlen(buffer);
+            for(int i=0;i<l;i++){
+                printf("%c",buffer[i]);
+            }
+              for(int i=0;i<l;i++){
+                if(buffer[i]>0){
+                    printf("%d ",buffer[i]);
+                    
+                }
+
+            }
+
 
         /*Make sure you comment every line */
         return 0;
         
 }
-
